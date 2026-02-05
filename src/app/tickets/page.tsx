848169092f3739dd726f0b4a44fafc66b00fe250
Rtanakar <ratnakarmishra@623gmail.com>
2026-02-05 20:26:25 +0530

    08: Const Assertions

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -2,11 +2,22 @@ import Link from "next/link";
 import { initialTickets } from "@/data";
 import { ticketPath } from "@/paths";
 
+// JavaScript object + dynamic key access
+// object[key] = us key ka value nikaalna
+// object = TICKET_ICONS
+// key = ticket.status
+const TICKET_ICONS = {
+  OPEN: "O",
+  IN_PROGRESS: ">",
+  DONE: "X",
+};
+
 const TicketsPage = () => {
   return (
     <div className="p-5">
       {initialTickets.map((ticket) => (
         <div key={ticket.id}>
+          <div>{TICKET_ICONS[ticket.status]}</div>
           <h2 className="text-lg">{ticket.title}</h2>
           <Link href={ticketPath(ticket.id)} className="text-sm underline">
             View
--- src/data.ts
+++ src/data.ts
@@ -1,15 +1,27 @@
+// 1️⃣ Const assertion kya hota hai?
+// TypeScript me as const ko hi const assertion kehte hain.
+// 👉 Iska kaam hota hai:
+// “Is value ko bilkul exact (literal) maan lo, broad type mat banao”
+
 export const initialTickets = [
   {
     id: "1",
     title: "ticket 1",
     content: "This is the first Ticket",
-    status: "DONE",
+    status: "DONE" as const,
   },
 
   {
     id: "2",
     title: "ticket 2",
     content: "This is the Second Ticket",
-    status: "OPEN",
+    status: "OPEN" as const,
+  },
+
+  {
+    id: "3",
+    title: "ticket 3",
+    content: "This is the Third Ticket",
+    status: "IN_PROGRESS" as const,
   },
 ];
