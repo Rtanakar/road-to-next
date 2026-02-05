fbb3fa44735187fb3d667fc44c3d90c871789f01
Rtanakar <ratnakarmishra@623gmail.com>
2026-02-05 19:55:04 +0530

    07: Script

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,17 +1,21 @@
 import { initialTickets } from "@/data";
 
-const TicketPage = async ({
-  params,
-}: {
-  params: Promise<{ ticketId: string }>;
-}) => {
+type TicketPageProps = {
+  params: Promise<{
+    ticketId: string;
+  }>;
+};
+
+const TicketPage = async ({ params }: TicketPageProps) => {
   const { ticketId } = await params;
+
   const ticket = initialTickets.find((ticket) => ticket.id === ticketId);
+
   if (!ticket) {
     return <h3>Page not found</h3>;
   }
 
-  return await (
+  return (
     <div>
       <h2 className="text-lg">{ticket.title}</h2>
       <p className="text-sm">{ticket.content}</p>
