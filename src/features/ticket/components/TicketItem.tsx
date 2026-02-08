af3485a6b4907bd49a0c6713b4e7e077c9d24486
Rtanakar <ratnakarmishra@623gmail.com>
2026-02-08 14:02:09 +0530

    23: Modify

--- src/features/ticket/components/TicketItem.tsx
+++ src/features/ticket/components/TicketItem.tsx
@@ -1,41 +1,47 @@
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import Link from "next/link";
 import { TICKET_ICONS } from "../constants";
 import { Ticket } from "../types";
 import { ticketPath } from "@/paths";
+import { Button } from "@/components/ui/button";
+import { SquareArrowOutUpRight } from "lucide-react";
 
 interface TicketItemProps {
   ticket: Ticket;
 }
 
 const TicketItem = ({ ticket }: TicketItemProps) => {
+  const detailButton = (
+    <Button variant="outline" size="icon" asChild>
+      <Link href={ticketPath(ticket.id)}>
+        <SquareArrowOutUpRight className="h-4 w-4" />
+      </Link>
+    </Button>
+  );
+
   return (
-    <Card className="w-full max-w-md">
-      <CardHeader>
-        <CardTitle className="flex gap-x-2 items-center">
-          <span>{TICKET_ICONS[ticket.status]}</span>
-          <span className="truncate">{ticket.title}</span>
-        </CardTitle>
-      </CardHeader>
+    <div className="w-full items-center flex max-w-105 gap-x-1">
+      <Card className="w-full">
+        <CardHeader>
+          <CardTitle className="flex gap-x-2">
+            <span>{TICKET_ICONS[ticket.status]}</span>
+            <span className="truncate">{ticket.title}</span>
+          </CardTitle>
+        </CardHeader>
 
-      <CardContent>
-        <span className="line-clamp-3 whitespace-break-spaces">
-          {ticket.content}
-        </span>
-      </CardContent>
+        <CardContent>
+          <span className="line-clamp-3 whitespace-break-spaces">
+            {ticket.content}
+          </span>
+        </CardContent>
+      </Card>
 
-      <CardFooter>
-        <Link href={ticketPath(ticket.id)} className="text-sm underline">
-          View
-        </Link>
-      </CardFooter>
-    </Card>
+      <div className="flex flex-col gap-y-1">
+        {detailButton}
+        {detailButton}
+        {detailButton}
+      </div>
+    </div>
   );
 };
 
