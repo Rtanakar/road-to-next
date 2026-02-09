9b571b7e312527ad43611b00c32d147ba183730c
Rtanakar <ratnakarmishra@623gmail.com>
2026-02-09 14:28:28 +0530

    33: Error Boundary

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,16 +1,20 @@
 import Heading from "@/components/heading/Heading";
 import SpinnerLoading from "@/features/ticket/components/SpinnerLoading";
 import TicketList from "@/features/ticket/components/TicketList";
+import { ErrorBoundary } from "react-error-boundary";
 import { Suspense } from "react";
+import Placeholder from "@/components/Placeholder";
 
 const TicketsPage = () => {
   return (
     <div className="flex flex-col flex-1 gap-y-8">
       <Heading title="Tickets" description="All your tickets at one place" />
 
-      <Suspense fallback={<SpinnerLoading />}>
-        <TicketList />
-      </Suspense>
+      <ErrorBoundary fallback={<Placeholder label="something went wrong!" />}>
+        <Suspense fallback={<SpinnerLoading />}>
+          <TicketList />
+        </Suspense>
+      </ErrorBoundary>
     </div>
   );
 };
--- src/features/ticket/queries/get-tickets.ts
+++ src/features/ticket/queries/get-tickets.ts
@@ -4,7 +4,9 @@ import { Ticket } from "../types";
 export const getTickets = async (): Promise<Ticket[]> => {
   await new Promise((resolve) => setTimeout(resolve, 2000));
 
-  return new Promise((resolve) => {
-    resolve(initialTickets);
-  });
+  throw new Error("something went wrong");
+
+  // return new Promise((resolve) => {
+  //   resolve(initialTickets);
+  // });
 };
