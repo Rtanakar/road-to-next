616ba1510f5d5c1d39d5c3ab92de4659a2204367
Rtanakar <ratnakarmishra@623gmail.com>
2026-02-09 08:05:33 +0530

    29: Streaming

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,22 +1,15 @@
 import Heading from "@/components/heading/Heading";
-import TicketItem from "@/features/ticket/components/TicketItem";
-import { getTickets } from "@/features/ticket/queries/get-tickets";
+import TicketList from "@/features/ticket/components/TicketList";
+import { Suspense } from "react";
 
-const TicketsPage = async () => {
-  const tickets = await getTickets();
-
+const TicketsPage = () => {
   return (
     <div className="flex flex-col flex-1 gap-y-8">
-      <Heading
-        title="TicketsPage"
-        description="All your tickets at one place"
-      />
+      <Heading title="Tickets" description="All your tickets at one place" />
 
-      <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-in-top">
-        {tickets.map((ticket) => (
-          <TicketItem key={ticket.id} ticket={ticket} />
-        ))}
-      </div>
+      <Suspense>
+        <TicketList />
+      </Suspense>
     </div>
   );
 };
---
+++ src/features/ticket/components/TicketList.tsx
@@ -0,0 +1,16 @@
+import { getTickets } from "../queries/get-tickets";
+import TicketItem from "./TicketItem";
+
+const TicketList = async () => {
+  const tickets = await getTickets();
+
+  return (
+    <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-in-top">
+      {tickets.map((ticket) => (
+        <TicketItem key={ticket.id} ticket={ticket} />
+      ))}
+    </div>
+  );
+};
+
+export default TicketList;
