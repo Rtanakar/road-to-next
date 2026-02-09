1025d9cfcb8bf79f89b7e0df45ea85b7379a0874
Rtanakar <ratnakarmishra@623gmail.com>
2026-02-09 18:52:46 +0530

    43: Router Cache

--- src/features/ticket/components/TicketItem.tsx
+++ src/features/ticket/components/TicketItem.tsx
@@ -16,7 +16,7 @@ interface TicketItemProps {
 const TicketItem = ({ ticket, isDetail }: TicketItemProps) => {
   const detailButton = (
     <Button variant="outline" size="icon" asChild>
-      <Link href={ticketPath(ticket.id)}>
+      <Link prefetch href={ticketPath(ticket.id)}>
         <SquareArrowOutUpRight className="h-4 w-4" />
       </Link>
     </Button>
